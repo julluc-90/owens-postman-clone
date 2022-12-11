00d108d9d006641b93ec44a39b7193cb68799907
julluc-90 <79543873+julluc-90@users.noreply.github.com>
2022-12-10 21:11:31 -0500

    finish query params

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+const queryParamsContainter = document.querySelector('[data-query-params]')
+const queryHeaderContainter = document.querySelector('[data-request-headers]')
+const keyValueTemplate = document.querySelector('[data-key-value-template]')
+
+document.querySelector('[data-add-query-param-btn]').addEventListener('click', (e) => {
+  queryParamsContainter.append(createKeyValuePair())
+})
+
+queryParamsContainter.append(createKeyValuePair())
+
+function createKeyValuePair() {
+  const element = keyValueTemplate.content.cloneNode(true)
+  element.querySelector('[data-remove-btn]').addEventListener('click', (e) => {
+    e.target.closest('[data-key-value-pair]').remove()
+  })
+  return element
+}
